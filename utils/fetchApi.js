9f52e3fbae4f5a1712ff4171d45d73f0498aeac5
Vugar Gafarli <development.vq@gmail.com>
2022-01-28 22:11:37 +0200

    fetching houses, continue later

---
+++ utils/fetchApi.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const baseUrl = 'https://bayut.p.rapidapi.com';
+
+export const fetchApi = async (url) => {
+    const { data } = await axios.get((url), {
+        headers: {
+            'x-rapidapi-host': 'bayut.p.rapidapi.com',
+            'x-rapidapi-key': '7ad0b9198amsh730438f396822e8p141363jsn8cd24221bbdc'
+        }
+    });
+    return data;
+}
