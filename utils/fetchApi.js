1c8c8c63cc077be6a7d75fc4a38b6e046ed67241
Vugar Gafarli <development.vq@gmail.com>
2022-01-30 17:41:11 +0200

    removed API key

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
             'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': "7ad0b9198amsh730438f396822e8p141363jsn8cd24221bbdc"
+            'x-rapidapi-key': process.env.REACT_APP_RAPID_KEY
         }
     });
     return data;
